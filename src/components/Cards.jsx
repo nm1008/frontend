efbf6845b35945d3d1629dddbbdfd06e282f4710
nm1008 <mallari08nikko@yahoo.com>
2023-10-05 11:42:53 +0800

    made cards reusable component / implemented cards reusable in home.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,13 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-export default function Cards() {
+export default function Cards({ name, description}) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title className="text-center">Card Title</Card.Title>
+        <Card.Title className="text-center">{name}</Card.Title>
         <Card.Text className='text-center' >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
-          earum consequatur fuga aliquam deserunt quasi facere cupiditate
-          tempora id asperiores?
+          {description}
         </Card.Text>
         <div className='text-center'>
           <Button variant="primary">Go somewhere</Button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
-import Cards from "../components/Cards"
+import { useEffect, useState } from "react";
+import Cards from "../components/Cards";
 
 export default function Home() {
+  const [courses, setCourses] = useState([]);
+
+  console.log(courses);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/courses")
+      .then((res) => res.json())
+      .then((data) => {
+        setCourses(data);
+      });
+  }, []);
+
   return (
     <>
       <div className="text-center m-5 pt-5">
@@ -11,9 +23,9 @@ export default function Home() {
         </button>
       </div>
       <div className="d-flex gap-5 align=items-center justify-content-center m-5 pt-5">
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards name={courses[0]?.name} description={courses[0]?.description} />
+        <Cards name={courses[1]?.name} description={courses[1]?.description} />
+        <Cards name={courses[2]?.name} description={courses[2]?.description} />
       </div>
     </>
   );
