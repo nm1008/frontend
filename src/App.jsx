147ce2206045dfe3b7294162c5094811d52de066
nm1008 <mallari08nikko@yahoo.com>
2023-10-07 11:03:01 +0800

    inc-adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {} from 'react-bootstrap'
 
 import Heading from "./components/Navbar";
 import Home from "./pages/Home";
@@ -7,12 +8,13 @@ import Courses from "./pages/Courses";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import EditProfile from "./pages/editProfile";
+import Footer from "./components/Footer"
 
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app h-100">
       <Heading />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,6 +23,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/EditProfile" element={<EditProfile />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import {} from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-center text-light py-1">
+      <h1>FOOTER</h1>
+    </footer>
+  );
+}
