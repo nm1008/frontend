7f102819942974fe159b6b78958afb8d5caa1777
nm1008 <mallari08nikko@yahoo.com>
2023-10-11 19:03:24 +0800

    finished add course

--- src/pages/AddCourse.jsx
+++ src/pages/AddCourse.jsx
@@ -1,64 +1,85 @@
-import { useEffect, useRef } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+const isAdmin = localStorage.getItem("isAdmin");
+const token = localStorage.getItem("token");
 
+export default function AddCourse() {
+  const [courseName, setCourseName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
 
+  const navigate = useNavigate()
 
-const addCourse = () => {
- 
-    const isAdmin = useRef(localStorage.getItem('isAdmin'))
+  const handleAddCourse = (e) => {
+    e.preventDefault();
+    try {
+      fetch(`http://localhost:3000/api/courses`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: courseName,
+          price: price,
+          description: description,
+        }),
+      })
+      .then((res) => {
+        if(res.ok){
+          alert('success')
+          navigate('/courses')
+        }else{
+          alert('failed')
+        }
+      })
+     
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    useEffect(() => {
-        isAdmin.current = isAdmin
-    })
-  
-   
   return (
     <>
-     
-     {console.log(isAdmin)}
-
-     {isAdmin === 'true' ? (
-         <div className="container my-5">
-         <h2 className="text-center my-5">Add Course</h2>
-         <form action="" >
-           <div className="d-flex flex-column align-items-center gap-3">
-             <div className="form-group col-md-6">
-               <label>Course Name</label>
-               <input
-                 type="text"
-                 className="form-control"
-           
-               />
-             </div>
-             <div className="form-group col-md-6">
-               <label>Price</label>
-               <input
-                 type="text"
-                 className="form-control"
-                
-               />
-             </div>
-             <div className="form-group col-md-6 h">
-               <label>Description</label>
-               <input
-                 type="text"
-                 className="form-control input-md"
-               
-               
-               />
-             </div>
-             <button className="col-md-6 col-sm-5 col-8 btn btn-primary">
-               Submit
-             </button>
-           </div>
-         </form>
-       </div>
-     ) : (
+      {isAdmin === "true" ? (
+        <div className="container my-5">
+          <h2 className="text-center my-5">Add Course</h2>
+          <form action="" onSubmit={handleAddCourse}>
+            <div className="d-flex flex-column align-items-center gap-3">
+              <div className="form-group col-md-6">
+                <label>Course Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => setCourseName(e.target.value)}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label>Price</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="form-group col-md-6 h">
+                <label>Description</label>
+                <input
+                  type="text"
+                  className="form-control input-md"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <button className="col-md-6 col-sm-5 col-8 btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : (
         <h1>Not available</h1>
-     )}
-       
+      )}
     </>
   );
-};
-
-export default addCourse;
+}
